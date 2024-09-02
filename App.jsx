f426971abbe30aea40e01a584899d974c63ac64b
Jaya Surya <162938075+jayasuryajayabal@users.noreply.github.com>
2024-09-02 19:03:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Chess from './chess'
+
+function App() {
+ 
+
+  return (
+    <>
+
+<Chess/>
+    </>
+  )
+}
+
+export default App
---
+++ chess.jsx
@@ -0,0 +1,120 @@
+import React from "react"
+import bac from './assets/chess.jpg';
+
+
+function Chess() {
+
+    return (
+        <>
+        <div className="total">
+            <div className="bak">
+               
+            </div>
+     
+            
+            <h1> J S chess board</h1>
+
+            <div className="main">
+
+
+    {/* image */}
+   
+                {/* row--1        */}
+
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+
+
+                {/* row--2        */}
+
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+
+
+                {/* row--3        */}
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+
+                {/* row--4        */}
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+
+                {/* row--5       */}
+
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+
+                {/* row--6        */}
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+
+
+                {/* row--7        */}
+
+                <div className="white"></div>
+                <div className="black">{ }</div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+
+                {/* row--8       */}
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+                <div className="black"></div>
+                <div className="white"></div>
+
+
+
+
+            </div>
+            </div>
+
+        </>
+    )
+}
+
+
+export default Chess
